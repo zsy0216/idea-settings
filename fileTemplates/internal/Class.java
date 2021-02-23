54c93fd989beeddf6497d7c97c82a4693812d276
zhangshuaiyin <shuaiyin.zhang@rivertech.com>
2021-02-23 10:01:41 +0800

    IU-2020.3.2 <ZSY@ZhangSan Create CodeGlance.xml, debugger.xml, lombok-plugin.xml, colors.scheme.xml, databaseDrivers.xml, updates.xml, Enum.java, Interface.java, rainbow_brackets.xml, editor.xml, Default.xml, ide.general.xml, project.default.xml, IntelliLang.xml, baseRefactoring.xml, shared-indexes.xml, wsl.distributions.xml, pluginAdvertiser.xml, _@user_Darcula.icls, markdown.xml, path.macros.xml, toStringTemplates.xml, laf.xml, grazie_global.xml, web-types-registry.xml, ide-features-trainer.xml, Default.xml, encoding.xml, yiiguxing.translation.xml, Class.java, jdk.table.xml, filetypes.xml, vcs.xml, mybatis.xml, find.xml, diff.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author: zhangshuaiyin
+ * @date: ${DATE} ${TIME}
+ */
+public class ${NAME} {
+}
